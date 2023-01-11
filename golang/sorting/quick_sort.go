e638a37bba6e7e7a28df8b4e9865384ca8428cbe
Patrick O'Shea <p14oshea@gmail.com>
2023-01-11 16:45:00 +0000

    add quick sort

---
+++ golang/sorting/quick_sort.go
@@ -0,0 +1,35 @@
+package sorting
+
+func qs(arr []int, low, high int) {
+	if low >= high {
+		return
+	}
+
+	pivotIdx := partition(arr, low, high)
+
+	qs(arr, pivotIdx+1, high)
+	qs(arr, low, pivotIdx-1)
+}
+
+func partition(arr []int, low, high int) int {
+	pivot := arr[high]
+	idx := low - 1
+	for i := low; i < high; i++ {
+		if arr[i] <= pivot {
+			idx++
+			tmp := arr[i]
+			arr[i] = arr[idx]
+			arr[idx] = tmp
+		}
+	}
+
+	idx++
+	arr[high] = arr[idx]
+	arr[idx] = pivot
+	return idx
+
+}
+func quick_sort(arr []int) []int {
+	qs(arr, 0, len(arr)-1)
+	return arr
+}
